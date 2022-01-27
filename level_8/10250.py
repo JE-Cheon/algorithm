b1e07bb903152873e52bf4793347f387393be308
JE-Cheon <ccj05080@konkuk.ac.kr>
2022-01-28 00:31:11 +0900

    1/28

--- level_8/10250.py
+++ level_8/10250.py
@@ -2,14 +2,7 @@ t = int(input())
 
 for i in range(t):
     h, w, n = map(int, input().split( ))
-    room_h = 1
-    room_w = 1
-    cnt = 0
-    for j in range(1,w+1):
-        for k in range(1,h+1):
-            if k <= h:
-                cnt += 1
-            room_h += 1
-        room_w += 1
-    if cnt == n:
-        print(room_h,room_w)
+    if n % h == 0:
+        print('{0}{1:02d}'.format(h, n//h))
+    else:
+        print('{0}{1:02d}'.format(n%h, (n//h)+1))
