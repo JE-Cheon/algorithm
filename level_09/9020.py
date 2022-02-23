6d9a77e9c452c45fa7e870aec487d666a2ed8b67
JE-Cheon <ccj05080@konkuk.ac.kr>
2022-02-24 01:40:26 +0900

    2/24

--- level_09/9020.py
+++ level_09/9020.py
@@ -6,14 +6,15 @@ for _ in range(t):
     a[1] = False
     lst = list(range(n + 1))
 
-    for i in range(2, int(n ** 0.5) + 1):
+    for i in range(2, int((n + 1) ** 0.5) + 1):
         if a[i] == True:
             for j in range(i + i, n + 1, i):
                 a[j] = False
     prime = [lst[i] for i in range(1,len(lst)) if a[i] == True]
-    part = []
-    for k in prime:
-        if (n-k) in prime:
-            if (n-2*k) >= 0:
-                part.append((k, n-k))
-    print(part[len(part)-1][0], part[len(part)-1][1],sep = ' ')
+    low = n//2
+    while True:
+        if low in prime and n-low in prime:
+            print(low, n-low, sep=' ')
+            break
+        else:
+            low -= 1
